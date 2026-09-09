b8e25f8967651546677c4487ec42dc30299d9867
Salah-Zain <salahzain36@gmail.com>
2026-09-09 16:16:38 +0530

    fix: add error catch to mongoose connect

--- config/Dbconnection.js
+++ config/Dbconnection.js
@@ -5,6 +5,8 @@ const ConnectDB = () => {
         const dbURI = process.env.MONGO_URI || 'mongodb://127.0.0.1/Scrab-PMK';
         mongoose.connect(dbURI).then(() => {
             console.log('Database is connected to:', dbURI.includes('127.0.0.1') ? 'Local MongoDB' : 'Cloud MongoDB');
+        }).catch((err) => {
+            console.error('Error connecting to MongoDB:', err.message);
         });
     } catch (error) {
         console.log(error, 'error in connecting config');
